eb892f412b18dfbc85d68f9d4d16ab5d02616e78
Walid Zattout <wzattout@gmail.com>
2020-04-25 04:12:31 +0200

    edit Dom Parser

--- src/eg/edu/alexu/csd/filestructure/btree/parser.java
+++ src/eg/edu/alexu/csd/filestructure/btree/parser.java
@@ -1,29 +1,39 @@
 package eg.edu.alexu.csd.filestructure.btree;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
+import javafx.util.Pair;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+import java.util.Objects;
 import javax.xml.parsers.ParserConfigurationException;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.traversal.DocumentTraversal;
-import org.w3c.dom.traversal.NodeFilter;
-import org.w3c.dom.traversal.NodeIterator;
+
 import org.xml.sax.SAXException;
 
 public class parser {
 
-    public ArrayList<String> parse(String fileName) throws ParserConfigurationException, IOException, SAXException {
-        ArrayList<String> content = new ArrayList<>();
+    public ArrayList<Pair<String, String>> parse(String fileName) throws ParserConfigurationException, IOException, SAXException {
+        ArrayList<Pair<String, String>> content = new ArrayList<>();
+        File xmlFile = new File(Objects.requireNonNull(getClass().getClassLoader().getResource(fileName)).getFile());
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder loader = factory.newDocumentBuilder();
-        Document document = loader.parse(String.valueOf(getClass().getClassLoader().getResource(fileName)));
-        DocumentTraversal trav = (DocumentTraversal) document;
-        NodeIterator it = trav.createNodeIterator(document.getDocumentElement(), NodeFilter.SHOW_ELEMENT, null, true);
-        for (Node node = it.nextNode(); node != null; node = it.nextNode()) {
-            String text = node.getTextContent();
-            content.add(text);
+        DocumentBuilder dBuilder = factory.newDocumentBuilder();
+        Document doc = dBuilder.parse(xmlFile);
+        doc.getDocumentElement().normalize();
+        NodeList nList = doc.getElementsByTagName("doc");
+        for (int i = 0; i < nList.getLength(); i++) {
+            Node nNode = nList.item(i);
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element elem = (Element) nNode;
+                String id = elem.getAttribute("id"), text = nNode.getTextContent();
+                content.add(new Pair<>(id, text));
+            }
         }
         return content;
     }
